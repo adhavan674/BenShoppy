420fdc4133fa827ac5015b2c5c632ca09e528cd4
Adhavan <adhavan6704@gmail.com>
2026-06-23 11:31:11 +0530

    improved WatchList Api and Swagger Documentation for WatchList APIs

--- src/main/java/com/adhavan/benshoppy/controller/WatchListController.java
+++ src/main/java/com/adhavan/benshoppy/controller/WatchListController.java
@@ -2,18 +2,24 @@ package com.adhavan.benshoppy.controller;
 
 import com.adhavan.benshoppy.dto.watchlist.SummaryProductResponseWatchList;
 import com.adhavan.benshoppy.service.WatchListService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Tag(name = "WatchList APIs" ,description = " User can assess this APIs")
+@SecurityRequirement(name = "bearerAuth")
 @RequestMapping("/watchlist")
+@RestController
 public class WatchListController {
 
     @Autowired
     private WatchListService watchListService;
 
+    @Operation(summary = "Add product to WatchList " )
     @PostMapping("/{userid}/{product_id}")
     public String addWatchList(@PathVariable Long userid,@PathVariable Long product_id){
 
@@ -21,21 +27,24 @@ public class WatchListController {
       return "added success full";
    }
 
-   @GetMapping("/{id}")
-    public List<SummaryProductResponseWatchList> getAllWatchList(@PathVariable Long id){
-        return watchListService.getWatchLists(id);
+   @Operation(summary = "Get All products in WatchList")
+   @GetMapping("/{user_id}")
+    public List<SummaryProductResponseWatchList> getAllWatchList(@PathVariable Long user_id){
+        return watchListService.getWatchLists(user_id);
 
    }
 
-   @DeleteMapping("/{id}")
-    public String removeWatchList(@PathVariable Long id){
-       watchListService.removeWatchList(id);
+   @Operation(summary = "Delete one product From WatchList")
+   @DeleteMapping("/{watchlist_id}")
+    public String removeWatchList(@PathVariable Long watchlist_id){
+       watchListService.removeWatchList(watchlist_id);
        return "removed successful";
    }
 
-   @DeleteMapping("/{id}/all")
-    public String removeWatchListAll(@PathVariable Long id){
-       watchListService.removeAll(id);
+   @Operation(summary = "Delete All products From WatchList")
+   @DeleteMapping("/all/{user_id}")
+    public String removeWatchListAll(@PathVariable Long user_id){
+       watchListService.removeAll(user_id);
        return "removed All ";
 
    }
